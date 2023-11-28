544c9ba342a77a5f5cbbe17161b596ccd85d4c5f
Rachel Kiarie <rachelnnk94@gmail.com>
2023-11-28 17:41:40 +0300

    Fetch current weather api url and key.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Search  from './components/search/search';
 import CurrentWeather from './components/current_weather/current_weather';
+import { Weather_API_URL, Weather_API_KEY } from './api';
 
 function App() {
   const handleOnSearchChange = (searchData) => {
-    console.log(searchData);
+    const [lat, lon] = searchData.value.split("");
+
+    const currentWeatherFetch = fetch(`${Weather_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${Weather_API_KEY }`)
 
   }
   return (
--- src/api.js
+++ src/api.js
@@ -1,4 +1,6 @@
 export const url = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
+export const Weather_API_URL = 'https://api.openweathermap.org/data/2.5';
+export const Weather_API_KEY = '2e3a20c59aa126d073ede2e33f306505';
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
